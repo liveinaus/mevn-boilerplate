d023674eb7ae98cbb55aa59edd45f2d676f2a17b
Sam Wang <admin@liveinaus.com>
2025-02-14 23:27:19 +1100

    feat: init commit

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,42 +1,37 @@
 export default {
-  imports: {
-    dirs: ['stores']
-  },
-  plugins: [
-    { src: '~/plugins/init.server.js' } // must be the first server plugin
-  ],
-  app: {
-    head: {
-      title: 'MEVN Stack Boilerplate - Client (Nuxt 3 with pinia)',
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Concert+One&family=Rubik:wght@300;400;600;800&display=swap'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'
-        }
-      ]
-    }
-  },
-  css: [
-    '~/assets/scss/mixins.scss',
-    '~/assets/css/main.css',
-    '~/assets/scss/main.scss',
-    '@fortawesome/fontawesome-svg-core/styles.css'
-  ],
-  modules: [
-    [
-      '@pinia/nuxt',
-      {
-        autoImports: [
-          // automatically imports `defineStore`
-          'defineStore', // import { defineStore } from 'pinia'
-          // automatically imports `defineStore` as `definePiniaStore`
-          ['defineStore', 'definePiniaStore'] // import { defineStore as definePiniaStore } from 'pinia'
-        ]
-      }
-    ]
-  ]
+	plugins: [
+		{ src: '~/plugins/init.server.js' }, // must be the first server plugin
+		{ src: '~/plugins/fontawesome.js', mode: 'client' }
+	],
+
+	app: {
+		head: {
+			title: 'MEVN Stack Boilerplate - Client (Nuxt 3 with Pinia)',
+			link: [
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/css2?family=Concert+One&family=Rubik:wght@300;400;600;800&display=swap'
+				}
+			]
+		}
+	},
+
+	css: [
+		'bootstrap/dist/css/bootstrap.min.css',
+		'~/assets/scss/mixins.scss',
+		'~/assets/css/main.css',
+		'~/assets/scss/main.scss',
+		'@fortawesome/fontawesome-svg-core/styles.css'
+	],
+
+	modules: ['@pinia/nuxt'],
+
+	pinia: {
+		autoImports: [
+			'defineStore', // import { defineStore } from 'pinia'
+			['defineStore', 'definePiniaStore']
+		]
+	},
+
+	compatibilityDate: '2025-02-14'
 }
